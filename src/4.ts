8579ea7e1af92f6801512a0920d54d79dad26c50
Serhii-Fesianov <littlefox06042000@gmail.com>
2024-02-05 21:11:13 +0200

    wait check

--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,63 @@
-const key = new Key();
+class Key {
+  private signature: number;
 
+  constructor() {
+    this.signature = Math.random();
+  }
+
+  getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person {
+  private key: Key;
+
+  constructor(key: Key) {
+    this.key = key;
+  }
+
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+abstract class House {
+  door: boolean = false;
+  protected key: Key;
+  tenants: Person[] = [];
+
+  comeIn(person: Person): void {
+    if (this.door) {
+      this.tenants.push(person);
+      console.log("Person came in.");
+    } else {
+      console.log("Door is closed.");
+    }
+  }
+
+  abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+  constructor(key: Key) {
+    super();
+    this.key = key;
+  }
+
+  openDoor(key: Key) {
+    if (this.key.getSignature() === key.getSignature()) {
+      this.door = true;
+      console.log("Door opened");
+    } else {
+      console.log("Invalid key");
+    }
+  }
+}
+
+const key = new Key();
 const house = new MyHouse(key);
 const person = new Person(key);
 
 house.openDoor(person.getKey());
-
 house.comeIn(person);
-
-
-export {};
